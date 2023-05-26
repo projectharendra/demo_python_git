cbbe112054e95bfcfb18a23a0a10f7783e4886b5
Harendra Kumar <project.harendra@gmail.com>
2023-05-26 11:33:10 +0530

    first commit3

--- myPythonFirst.py
+++ myPythonFirst.py
@@ -1,2 +1,12 @@
-print ('Harendra')
-print (2 + 2)
+import mysql.connector
+
+mydb = mysql.connector.connect(auth_plugin='mysql_native_password',host="localhost",user='sa',password="Passw0rd",database="demo_db");
+mycursor = mydb.cursor()
+
+#mycursor.execute("show databases")
+
+mycursor.execute("select * from student")
+#result = mycursor.fetchall()
+result = mycursor.fetchone()
+for i in result:
+	print(i)
